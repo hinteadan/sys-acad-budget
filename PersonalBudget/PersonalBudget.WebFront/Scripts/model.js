e71d6488d34953aa1626ae0726406303226e8a03
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-03-19 13:00:04 +0200

    Some basic application model

---
+++ PersonalBudget/PersonalBudget.WebFront/Scripts/model.js
@@ -0,0 +1,24 @@
+﻿(function (undefined) {
+    'use strict';
+
+    //We define our application model
+    function Income(amount, date, info) {
+        this.amount = amount;
+        this.date = date || new Date();
+        this.info = info || null;
+    }
+
+    function Spending(amount, date, info) {
+        this.amount = amount;
+        this.date = date || new Date();
+        this.info = info || null;
+    }
+
+
+    //Now we expose what we want, how we want, to the "outside world"
+    this.model = {
+        Income: Income,
+        Spending: Spending
+    };
+
+}).call(this);
