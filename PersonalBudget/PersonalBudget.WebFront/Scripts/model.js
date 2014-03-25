c55956d5ea1147e0f33eb6c0e361c593be1d2590
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-03-25 16:33:50 +0200

    Define basic app model

---
+++ PersonalBudget/PersonalBudget.WebFront/Scripts/model.js
@@ -0,0 +1,26 @@
+﻿(function (undefined) {
+    'use strict';
+
+    function Income() {
+        var privateAmount;//equiv. pt. private int abcd = 0;
+        this.amount = 0;//equiv pt. public int defg = 0;
+        this.timestamp = new Date();
+        this.notes = null;
+        this.category = null;
+    }
+
+    function Spending() {
+        this.amount = 0;
+        this.timestamp = new Date();
+        this.notes = null;
+        this.category = null;
+    }
+
+    function Category() {
+        this.name = null;
+        this.iconUrl = null;
+    }
+
+    
+
+}).call(this);
